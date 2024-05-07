77b4c474bdd196ed8d6f21ce089a37b7f4ab59b9
Michał Przyłucki <michal.przyl@gmail.com>
2024-05-08 00:07:38 +0200

    Refactor initial tests

--- testing.py
+++ testing.py
@@ -1,5 +1,5 @@
 import pytest
-from node import LinkedList, Node
+from node import LinkedList
 
 
 def test_create_linked_list():
@@ -10,34 +10,35 @@ def test_create_linked_list():
 class TestAddingNodes:
 
     @pytest.fixture
-    def setup(self):
-        self.l_list = LinkedList()
+    def l_list(self):
+        l_list = LinkedList()
+        return l_list
 
-    def test_l_list_with_one_element(self, setup):
-        self.l_list.add_node("hello")
-        assert self.l_list.first_node.value == "hello"
-        assert self.l_list.last_node.value == "hello"
+    def test_l_list_with_one_element(self, l_list):
+        l_list.add_node("hello")
+        assert l_list.first_node.value == "hello"
+        assert l_list.last_node.value == "hello"
 
-    def test_l_list_with_two_elements_head_and_tail(self, setup):
-        self.l_list.add_node("hello")
-        self.l_list.add_node("world")
-        assert self.l_list.first_node.value == "hello"
-        assert self.l_list.last_node.value == "world"
+    def test_l_list_with_two_elements_head_and_tail(self, l_list):
+        l_list.add_node("hello")
+        l_list.add_node("world")
+        assert l_list.first_node.value == "hello"
+        assert l_list.last_node.value == "world"
 
-    def test_l_list_with_two_elements_pointer_to_next(self, setup):
-        self.l_list.add_node("hello")
-        self.l_list.add_node("world")
-        assert self.l_list.first_node.next_node == self.l_list.last_node
+    def test_l_list_with_two_elements_pointer_to_next(self, l_list):
+        l_list.add_node("hello")
+        l_list.add_node("world")
+        assert l_list.first_node.next_node == l_list.last_node
 
-    def test_l_list_with_three_elements_head_and_tail(self, setup):
-        self.l_list.add_node("hello")
-        self.l_list.add_node("world")
-        self.l_list.add_node("whatsup")
-        assert self.l_list.first_node.value == "hello"
-        assert self.l_list.last_node.value == "whatsup"
+    def test_l_list_with_three_elements_head_and_tail(self, l_list):
+        l_list.add_node("hello")
+        l_list.add_node("world")
+        l_list.add_node("whatsup")
+        assert l_list.first_node.value == "hello"
+        assert l_list.last_node.value == "whatsup"
 
-    def test_l_list_with_three_elements_pointer_to_next(self, setup):
-        self.l_list.add_node("hello")
-        self.l_list.add_node("world")
-        self.l_list.add_node("whatsup")
-        assert self.l_list.first_node.next_node.value == "world"
+    def test_l_list_with_three_elements_pointer_to_next(self, l_list):
+        l_list.add_node("hello")
+        l_list.add_node("world")
+        l_list.add_node("whatsup")
+        assert l_list.first_node.next_node.value == "world"
